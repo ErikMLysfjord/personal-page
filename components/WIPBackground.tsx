cd360ec75bce0ba899b647ee53d925ce3c0617bc
Erik Menkin Lysfjord <erikmly@smint.no>
2022-12-29 00:23:30 +0100

    refactor: change colourscheme

--- components/WIPBackground.tsx
+++ components/WIPBackground.tsx
@@ -1,14 +1,11 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
-import ImageForBackground from '../public/IMG_1914.jpeg';
+import React from 'react';
 
 function WIPBackground() {
   return (
-    <div
-      className='h-full w-full flex justify-center items-center'
-      style={{ backgroundColor: 'rgb(64, 54, 77)' }}
-    >
-      <div className='z-10 relative text-center text-7xl'>Work in Progress</div>
+    <div className='h-full w-full flex justify-center items-center bg-zinc-900'>
+      <div className='animate-bounce z-10 relative text-center text-7xl text-white dark:text-gray-900'>
+        Work in Progress
+      </div>
     </div>
   );
 }
