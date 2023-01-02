829e6e1d7ea334d060f6d96fb54bcb4d124a920e
Erik Menkin Lysfjord <erik.menkin@lysfjord.no>
2023-01-03 00:47:13 +0100

    feat: add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import type { NextPage } from 'next';
 import WIPBackground from '../components/WIPBackground';
 import Navbar from '../components/Navbar';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
-    <div className='h-screen w-screen bg-slate-300 dark:bg-zinc-900'>
-      <Navbar />
-      <WIPBackground />
-    </div>
+    <>
+      <Head>
+        <title>Erik Menkin Lysfjord</title>
+      </Head>
+      <div className='h-screen w-screen bg-slate-300 dark:bg-zinc-900'>
+        <Navbar />
+        <WIPBackground />
+      </div>
+    </>
   );
 };
 
