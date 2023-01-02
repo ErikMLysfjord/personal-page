cb88df7647672aa3f0db1f74b1494887ebb5d89d
Erik Menkin Lysfjord <erik.menkin@lysfjord.no>
2023-01-03 00:47:30 +0100

    feat: add responsive breakpoints

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,10 +7,10 @@ function Navbar() {
 
   return (
     <div
-      className='w-full bg-slate-400 dark:bg-zinc-700 sticky top-0 h-16 opacity-95 z-20 text-white text-center h-16 p-0 text-black dark:text-white'
+      className='w-full sticky dark:border-slate-700/30 border-slate-900/10 border-b top-0 h-16 opacity-95 z-20 text-white text-center p-0 text-black dark:text-white backdrop-blur-sm'
       style={{ lineHeight: '4rem' }}
     >
-      <div className='flex flex-row align-middle justify-between lg:max-w-4xl mx-auto'>
+      <div className='flex flex-row align-middle justify-between md:max-w-2xl lg:max-w-4xl max-w-xs mx-auto'>
         <p>Heisann!</p>
         <div>{date}</div>
         <DarkmodeButton />
